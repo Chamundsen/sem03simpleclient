a7d139449253de9bae21edd282ea9e4fe3029ddb
Chamundsen <simencamu@hotmail.com>
2023-05-03 15:31:11 +0000

    fikset

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Chamundsen/sem03simpleclient
+
+go 1.18
+
+require github.com/Chamundsen/is105sem03 v0.0.0-20230502130918-c2474858ea15
--- tcp_client.go
+++ tcp_client.go
@@ -4,25 +4,34 @@ import (
 	"net"
 	"log"
 	"os"
+"github.com/Chamundsen/is105sem03/mycrypt"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:")
+	conn, err := net.Dial("tcp", "172.17.0.5:6060")
 	if err != nil {
 		log.Fatal(err)
 	}
     
 	log.Println("os.Args[1] = ", os.Args[1])
 
- 	_, err = conn.Write([]byte(os.Args[1]))
-	if err != nil {
-		log.Fatal(err)
-	}
+
+ 	  kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), 
+mycrypt.ALF_SEM03, 4)
+    
+
+    
+
+    _, err = conn.Write([]byte(string(kryptertMelding)))
+    if err != nil {
+        log.Fatal(err)
+    }
+	
 	buf := make([]byte, 1024)
 	n, err := conn.Read(buf)
 	if err != nil {
 		log.Fatal(err)
 	} 
-	response := string(buf[:n])
-	log.Printf("reply from proxy: %s", response)
+	kryptertRespons := buf[:n]
+	log.Printf("kryptert respons: %s", kryptertRespons)
 }
